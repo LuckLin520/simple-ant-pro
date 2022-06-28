cee9f1c20c7bc3993d17bff2da1b812157d2239b
LuckLin520 <33896086+LuckLin520@users.noreply.github.com>
2022-06-28 15:40:42 +0800

    Update bootstrap.js

--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -3,6 +3,8 @@ import { updateTheme } from '@ant-design-vue/pro-layout'
 import store from '@/store'
 import defaultSetting from './defaultSetting'
 
-let settings = storage.get('settings') || defaultSetting
-updateTheme(settings.primaryColor)
-store.dispatch('common/saveSettings', settings)
+export default () => {
+  let settings = storage.get('settings') || defaultSetting
+  updateTheme(settings.primaryColor)
+  store.dispatch('common/saveSettings', settings)
+}
